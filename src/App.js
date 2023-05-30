d6dbc42370b39a56c72625521b22c7d2fe9110f0
Zarema Kharachyk <zkharachyk@LABA.PSWAS.LOCAL>
2023-05-30 16:39:11 -0700

    add

--- src/App.js
+++ src/App.js
@@ -1,9 +1,50 @@
 import Header from "./Components/Header";
+import Tasks from "./Components/Tasks";
+import { useState } from "react"
 
 function App() {
+  const [tasks, setTasks] = useState(
+    [
+        {
+            id: 1,
+            text: 'Doctors Appointment',
+            day: 'July 5th at 3:00pm',
+            reminder: true,
+        },
+        {
+            id: 2,
+            text: 'Meeting at school',
+            day: 'July 7th at 1:00pm',
+            reminder: true,
+        },
+        {
+            id: 3,
+            text: 'Food Shopping',
+            day: 'Today',
+            reminder: false,
+        },
+        ])
+
+        //Delete Task
+    const deleteTask = (id) => {
+      // console.log('delete', id)
+      setTasks(tasks.filter((task) => task.id !== id))
+    }
+    
+    //Toggle Reminder
+    const toggleReminder = (id) => {
+      console.log(id)
+    }
+
   return (
     <div className="container">
-      <Header title="hello"/>
+      <Header />
+      {tasks.length > 0 ? (
+      <Tasks tasks={tasks} onDelete={deleteTask}
+      onToggle={toggleReminder}/>
+      ) : (
+        "Let's go to the Beach! :)"
+      )}
     </div>
   );
 }
